d249b90c088f4ce97775725adf24a5718f2f756c
Udayakumar <infotoudaya@gmail.com>
2026-02-22 21:50:21 +0530

    cleanup remove utils from test project

--- project-orangehrm/utils/loginHelper.ts
+++
@@ -1,9 +0,0 @@
-// projectA/utils/loginHelper.ts
-import { Page } from "@playwright/test";
-
-export async function performLogin(page: Page) {
-  await page.goto("https://projectA.example.com/login");
-  await page.fill("#userId", "projectAUser");
-  await page.fill("#pwd", "projectAPassword");
-  await page.click("button.login");
-}
